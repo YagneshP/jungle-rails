f528049a525124535e3fd2346dda08482a1418ca
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-06 16:49:48 -0500

    Add about index route

--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,20 @@ Rails.application.routes.draw do
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
-
+  resources :about, only: [:index]
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
   end
 
   resources :orders, only: [:create, :show]
-
+ 
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
   end
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
