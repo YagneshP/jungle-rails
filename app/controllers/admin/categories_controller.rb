d42275e96cb3b604223e683ef7e9adfefc7d8f0b
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-06 19:04:29 -0500

    Getting all categories through index

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,6 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASSWORD']
+  def index
+    @categories = Category.all
+  end
 end
