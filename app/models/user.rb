468066a2df9f2fde16c9f98e70a682c4ce0690cd
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-11 10:20:54 -0500

    Add validation for the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
+  
+  validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+  validates :name, presence: true
 end
