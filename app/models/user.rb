404bf317ae18a95206db28a889821032b149ef1e
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-11 18:04:09 -0500

    Add method for checking credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,17 @@
 class User < ActiveRecord::Base
   has_secure_password
-  
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
   validates :name, presence: true
+ 
+  def self.authenticate_with_credentials (email, password)
+    email = email.downcase.strip
+    user = User.find_by("lower(email) =?", email)
+    if user && user.authenticate(password) 
+      user
+    else
+      nil
+    end
+  end
 end
