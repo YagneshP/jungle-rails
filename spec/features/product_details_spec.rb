34437a68ed716b7eaa8d7fa368681c50d7510f0c
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-11 22:46:42 -0500

    Add scenario for product detail page

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "navigate to product_detail page" do
+    # ACT
+    visit root_path
+    page.find(".product:nth-child(1) .actions .btn-default").click
+    
+    # save_screenshot
+
+    # VERIFY
+    expect(page).to have_css 'article.product-detail', count: 1
+  end
 end
